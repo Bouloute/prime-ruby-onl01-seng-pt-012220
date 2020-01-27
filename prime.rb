aeeb2d0aaa11c4f7ff26ac064d9b8f9403c99254
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-27 16:57:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  #0, 1 and degatives aren't prime numbers
+  if (n <=1)
+     return false
+  end
+
+  # a prime is dividable by 1 and itself (you know what I mean)
+  i = 2
+  while(i<n)
+    if ((n % i) == 0)
+      return false
+    end
+    i += 1
+  end
+  return true
+end
